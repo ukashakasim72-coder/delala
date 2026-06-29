404b5437ef309aed023fd1d8df7dfeb05e4bdf55
ukashakasim72-coder <ukashakasim72@gmail.com>
2026-06-29 09:56:57 +0300

    Update settings.py

--- agent_site/settings.py
+++ agent_site/settings.py
@@ -46,7 +46,7 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest9_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
 }
 from datetime import timedelta
