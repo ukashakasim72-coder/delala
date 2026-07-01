2e2b509f404180f0f1e23e5fbc326cf6dc992683
ukashakasim72-coder <ukashakasim72@gmail.com>
2026-07-01 09:47:15 +0300

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+# Exit on error
+set -e
+
+echo "Installing dependencies..."
 pip install -r requirements.txt
 
-python manage.py collectstatic --noinput
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear
 
-python manage.py migrate
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+echo "Build complete!"
